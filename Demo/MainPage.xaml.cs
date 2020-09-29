97b6e9a2b83cd54aa248bad576b8b968ff7e8c48
Altino Júnior <altinoalvesjunior@gmail.com>
2020-09-29 13:37:00 -0300

    Coreções

---
+++ Demo/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenScanner(object sender, EventArgs e)
+        {
+            Scanner();
+        }
+
+        public async void Scanner()
+        {
+
+            var ScannerPage = new ZXingScannerPage();
+
+            ScannerPage.OnScanResult += (result) => {
+                // Parar de escanear
+                ScannerPage.IsScanning = false;
+
+                // Alert com o código escaneado
+                Device.BeginInvokeOnMainThread(() => {
+                    Navigation.PopAsync();
+                    DisplayAlert("Código escaneado", result.Text, "OK");
+                });
+            };
+
+            
+            await Navigation.PushAsync(ScannerPage);
+
+        }
+    }
+}
