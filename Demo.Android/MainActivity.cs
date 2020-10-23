5b9377a1f5764746697db8da1677bb92fe6d6bd5
Altino <44076017+altinoalvesjunior@users.noreply.github.com>
2020-10-23 13:49:51 -0300

    Update MainActivity.cs

--- Demo.Android/MainActivity.cs
+++ Demo.Android/MainActivity.cs
@@ -20,7 +20,7 @@ namespace Demo.Droid
 
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init();
+            global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
         
